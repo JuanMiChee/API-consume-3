c09d1662d9cfd04679aac1c115fd90ed3693b091
JuanMiChee <harringtonjuan@outlook.com>
2021-11-11 17:24:08 -0400

    actualización de links y esfera de carga de archivos

--- script.js
+++ script.js
@@ -1,68 +1,82 @@
-async function fetchTimeApi() {
+async function fetchCryptoApi() {
     let url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
     let response = await fetch(url);
     return response.json();
 }
 
+function showAll(){
+    fetchCryptoApi().then(jsonProcessing);
+    toggleLoad();
+    
+}
+showAll();
+let loaderHide = document.getElementById("loader");
+loaderHide.style.display = 'block';
 
-fetchTimeApi().then(jsonProcessing)
+function toggleLoad(){
+    let loader = document.getElementById("loader");
+    if (loader.style.display === "none") {
+      loader.style.display = "block";
+    } else {
+      loader.style.display = "none";     
+       }
+    }
 
 function jsonProcessing(json) {         
-        let timeJsonObject = json.time.updated;
+    let timeJsonObject = json.time.updated;       
+    showEurInformation(json);
+    showUsdInformation(json);
+    showGbpInformation(json); 
+    toggleLoad();
+    console.log(json);       
+} 
 
-        let jsonData = {
-            updated: json.time.time,
-            disclaimer: json.disclaimer,
-            bpi: json.bpi,
-            gbp: json.bpi.USD,
-            eur: json.bpi.EUR,
-        }
-        console.log(jsonData);  
-         
-        // EUR SECTION:
+function showEurInformation(json){
+    // EUR SECTION:
+    let copyright = document.getElementById("copyright");
+    copyright.innerHTML = "" + json.disclaimer;
 
-        
+    let general = document.getElementById("euro1");
+    general.innerHTML =  json.bpi.EUR.rate + " Eur Rate";
 
-        let copyright = document.getElementById("copyright");
-        copyright.innerHTML = "" + jsonData.disclaimer;
+    let rateFloat = document.getElementById("euro2");
+    rateFloat.innerHTML =  json.bpi.EUR.rate_float + " Eur Rate_float";
 
-        let general = document.getElementById("euro1");
-        general.innerHTML =  json.bpi.EUR.rate + " Eur Rate";
+    let symbol = document.getElementById("euro3");
+    symbol.innerHTML = "" + json.bpi.EUR.symbol;
+    // EUR SECTION
+}       
 
-        let rateFloat = document.getElementById("euro2");
-        rateFloat.innerHTML =  json.bpi.EUR.rate_float + " Eur Rate_float";
+    function showUsdInformation(json){
+    // USD SECTION:
+    let usdRate = document.getElementById("USD1");
+    usdRate.innerHTML =  json.bpi.USD.rate + " USD Rate";
 
-        let symbol = document.getElementById("euro3");
-        symbol.innerHTML = "" + json.bpi.EUR.symbol;
+    let usdRateFloat = document.getElementById("USD2");
+    usdRateFloat.innerHTML =  json.bpi.USD.rate_float  + " USD Rate_float";
 
-        // EUR SECTION
-
-
-        // USD SECTION:
-
-        let usdRate = document.getElementById("USD1");
-        usdRate.innerHTML =  json.bpi.USD.rate + " USD Rate";
-
-        let usdRateFloat = document.getElementById("USD2");
-        usdRateFloat.innerHTML =  json.bpi.USD.rate_float  + " USD Rate_float";
-
-        let usdSymbol = document.getElementById("USD3");
-        usdSymbol.innerHTML = "" + json.bpi.USD.symbol;
-
-        // USD SECTION
-
-        // gbp SECTION:
-        let gbpRate = document.getElementById("gbp1");
-        gbpRate.innerHTML = json.bpi.GBP.rate +"GBP Rate ";
-
-        let gbpRateFloat = document.getElementById("gbp2");
-        gbpRateFloat.innerHTML =  json.bpi.GBP.rate_float +"GBP Rate_float " ;
-
-        let gbpSymbol = document.getElementById("gbp3");
-        gbpSymbol.innerHTML = "" + json.bpi.GBP.symbol;
-
-        // gbp SECTION
+    let usdSymbol = document.getElementById("USD3");
+    usdSymbol.innerHTML = "" + json.bpi.USD.symbol;
+    // USD SECTION
 }
+   
+
+    function showGbpInformation(json){
+    // gbp SECTION:
+    let gbpRate = document.getElementById("gbp1");
+    gbpRate.innerHTML = json.bpi.GBP.rate +"GBP Rate ";
+
+    let gbpRateFloat = document.getElementById("gbp2");
+    gbpRateFloat.innerHTML =  json.bpi.GBP.rate_float +"GBP Rate_float " ;
+
+    let gbpSymbol = document.getElementById("gbp3");
+    gbpSymbol.innerHTML = "" + json.bpi.GBP.symbol;
+    // gbp SECTION
+
+}
+
+
+
 
 
    
